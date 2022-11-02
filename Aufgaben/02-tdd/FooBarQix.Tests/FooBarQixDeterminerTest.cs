9aed02ed77b86af38e78a290a02695d30693836b
Elias <elias_amstein@sluz.ch>
2022-11-02 09:29:05 +0100

    Aufgabe 1 TDD

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,47 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determine_canBeDevidedBy3_getFoo()
         {
+            int a = 33;
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            Assert.Equal("Foo", actual);
+        }
+
+        [Fact]
+        public void Determine_canBeDevidedBy3and5_getFoo()
+        {
+            int a = 90;
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            Assert.Equal("FooBar", actual);
+        }
+
+        [Fact]
+        public void Determine_canBeDevidedBy7_getFoo()
+        {
+            int a = 28;
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            Assert.Equal("Qix", actual);
+        }
+
+        [Fact]
+        public void Determine_canBeDevidedBy2and5and7_getFoo()
+        {
+            int a = 105;
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            string actual = fooBarQixDeterminer.Determine(a);
+
+            Assert.Equal("FooBarQix", actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,18 @@
 {
     public class FooBarQixDeterminer
     {
-
+        public string Determine(int input) {
+            string sentence = "";
+            if (input % 3 == 0) {
+                sentence = sentence + "Foo";
+            }
+            if (input % 5 == 0) {
+                sentence = sentence + "Bar";
+            }
+            if (input % 7 == 0) {
+                sentence = sentence + "Qix";
+            }
+            return sentence;
+        }
     }
 }
