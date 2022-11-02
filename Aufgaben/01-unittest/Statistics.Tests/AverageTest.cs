c63f03cd119819e61d959d0ac7be3359ecc830ae
Elias <elias_amstein@sluz.ch>
2022-11-02 08:44:56 +0100

    Aufgaben

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,13 +9,34 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 5, 3 };
+            double expected = 4;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 5, 3, 4, 5, 6 };
+            double expected = 4.6;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfZero()
+        {
+            List<int> numbers = new List<int> { };
+            Average average = new Average();
+
+            Assert.Throws<DivideByZeroException>(() => average.Mean(numbers));
         }
     }
 }
